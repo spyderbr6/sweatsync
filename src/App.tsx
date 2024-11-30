2a087d6f28fd99346610c8c71deee1a0eeb27609
Dean <dbursic+git@gmail.com>
2024-11-30 15:37:57 -0500

    initial sync

--- src/App.tsx
+++ src/App.tsx
@@ -26,13 +26,7 @@ function App() {
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+
     </main>
   );
 }
