ba10ee4e7713925f8d22bca06f7d86ea9431c071
Dean <dbursic+git@gmail.com>
2024-11-30 15:51:59 -0500

    add storage. reformat to workout

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,10 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  PostforWorkout: a
     .model({
       content: a.string(),
+      url: a.string()
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [workoutposts, setworkoutposts] = useState<Array<Schema["PostforWorkout"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.PostforWorkout.observeQuery().subscribe({
+      next: (data) => setworkoutposts([...data.items]),
     });
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    client.models.PostforWorkout.create({ content: window.prompt("Todo content") });
   }
 
   return (
@@ -22,8 +22,8 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {workoutposts.map((PostforWorkout) => (
+          <li key={PostforWorkout.id}>{PostforWorkout.content}</li>
         ))}
       </ul>
 
