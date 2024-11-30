23ed901543266c1cbed007fc1cbea38087414193
spyderbr6 <bursicd@live.com>
2024-11-30 17:17:26 -0500

    Revert "Addinputfields"

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
+
 export const storage = defineStorage({
-    name: 'amplifyTeamDrive',
-    access: (allow) => ({
-      'picture-submissions/*': [
-        allow.authenticated.to(['read','write']),
-        allow.guest.to(['read', 'write'])
-      ],
-    })
-  });
+  name: 'amplifyTeamDrive'
+});
