55c68ca6a6e1ea511863bd3a0a9042f46c12a2f9
Dean <dbursic+git@gmail.com>
2025-01-03 09:38:07 -0500

    hard coded email for test

--- amplify/functions/sendNotificationFunction/resource.ts
+++ amplify/functions/sendNotificationFunction/resource.ts
@@ -6,7 +6,7 @@ export const sendPushNotificationFunction = defineFunction({
   name: 'sendPushNotificationFunction', 
   entry: './handler.ts',
   environment: {
-    VAPID_EMAIL: process.env.VITE_VAPID_EMAIL ?? '',
+    VAPID_EMAIL: process.env.VITE_VAPID_EMAIL ?? 'mailto:bursicd+vapid@live.com',
     VAPID_PUBLIC_KEY: process.env.VITE_VAPID_PUBLIC_KEY ?? '',
     VAPID_PRIVATE_KEY: process.env.VITE_VAPID_PRIVATE_KEY ?? ''
   }
